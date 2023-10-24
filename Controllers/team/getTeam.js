2a33b06e269a4b857e25171c6fdd56f2ded22b83
thanos101201 <ayushsingh10122001@gmail.com>
2023-10-24 17:04:19 +0530

    updated the controller for getplayer and getteam

--- Controllers/team/getTeam.js
+++ Controllers/team/getTeam.js
@@ -1,18 +1,27 @@
+const playerModel = require("../../model/player");
 const teamModel = require("../../model/team");
 
 const getTeam = (req, res) =>{
     const id = req.headers.id
     teamModel.find({
         _id: id
-    }).then((resp1) => {
+    }).then(async (resp1) => {
         if(resp1.length === 0){
             res.status(204).send()
         }
         else{
-            res.status(200).send({
-                'message': 'Team data is here',
-                'data': resp1
-            });
+            await playerModel.find({
+                email: {
+                    $in : resp1[0].players
+                }
+            }).then((resp2) => {
+                res.status(200).send({
+                    'message': 'Team data is here',
+                    'data': resp2
+                });
+            }).catch((er2) => {
+                res.status(400).send(er2);
+            })
         }
     }).catch((er1) => {
         res.status(400).send(er1);
