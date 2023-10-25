ecb74cefb2d93b28870212922747c065365b0912
thanos101201 <ayushsingh10122001@gmail.com>
2023-10-25 23:32:23 +0530

    updated the update team route

--- Controllers/team/updateTeam.js
+++ Controllers/team/updateTeam.js
@@ -1,21 +1,45 @@
-const axios = require('axios');
 const teamModel = require('../../model/team');
+const playerModel = require('../../model/player');
 
 
 const updateTeam = (req, res) => {
     const name = req.body.name;
-    const id = req.body.id;
-    teamModel.updateOne({
-        _id: id
-    }, {
-        name: name
+    const email = req.body.email;
+    playerModel.find({
+        email: email
     }).then((resp1) => {
-        res.status(200).send({
-            'message': 'Name updated'
-        });
+        console.log(resp1);
+        if(resp1.length === 0){
+            res.status(204).send();
+        }
+        else{
+            teamModel.updateOne({
+                _id: resp1[0].teamId
+            }, {
+                name: name
+            }).then((resp2) => {
+                console.log(resp2);
+                res.status(200).send({
+                    'message': 'Team Name updated'
+                });
+            }).catch((er2) => {
+                res.status(400).send(er2);
+            })
+        }
     }).catch((er1) => {
         res.status(400).send(er1);
-    });
+    })
+    // teamModel.updateOne({
+    //     _id: id
+    // }, {
+    //     name: name
+    // }).then((resp1) => {
+    //     res.status(200).send({
+    //         'message': 'Name updated'
+    //     });
+    // }).catch((er1) => {
+    //     res.status(400).send(er1);
+    // });
 }
 
 module.exports = updateTeam;
